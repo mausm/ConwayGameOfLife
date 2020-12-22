d66cc5c5d1a10943b380dfb28c832bc202ea0eed
mausm <maurits4ever@hotmail.com>
2020-12-22 17:02:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 
 descStr = "Conway's game of life:" \
        "Simulated by Python with randomized or user defined starting points"
